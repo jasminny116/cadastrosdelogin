cb4c0807cf14a7b95a14572cfdf9738678dd9af5
minny <jasminny61@gmail.com>
2024-09-06 03:29:47 +0800

    Update cadrastrosdelogin.py

--- cadrastrosdelogin.py
+++ cadrastrosdelogin.py
@@ -6,13 +6,13 @@ senha = []
 
 
 while True:
-  print("************")
+  print("*****************************")
   print("Bem vindo ao sistema de login")
   print("1 - Cadastrar")
   print("2 - Ver logins salvos")
   print("3 - Limpar tela")
   print("4 - Sair")
-  print("************")
+  print("*****************************")
 
   opcao = input("Escolha uma opção: ")
 
@@ -26,7 +26,7 @@ while True:
     senha.append(senha_digitada)
     novamente = input("Deseja cadastrar outra conta? (s/n):")
     if novamente.lower() == "n":
-      os.system('clear')
+      os.system('cls')
     elif novamente.lower() == "s":
       print("Digite seu email:")
       login = input("")
@@ -37,24 +37,24 @@ while True:
       senha.append(senha_digitada)	
       print("senhas adicionadas com sucesso!")
       time.sleep(2)
-      os.system('clear')
+      os.system('cls')
     else:
       print("*use apenas S ou N!*")
       time.sleep(2)
-      os.system('clear')
+      os.system('cls')
 
 
   elif opcao == "2":
-    os.system('clear')
+    os.system('cls')
     print("Emails cadastrados(Veja os números correspondentes):")
     for i, email in enumerate(email):
       print(f"{i+1}. {email}")
     print("Senhas cadastradas:")
     for i, senha in enumerate(senha):
       print(f"{i+1}. {senha}")
-    print("****************")
+    print()
   elif opcao == "3":
-    os.system('clear')
+    os.system('cls')
   elif opcao == "4":
     print("tem certeza que deseja sair?Todos seus dados serão perdidos!")
     print("1 - sim")
@@ -62,20 +62,20 @@ while True:
     
     decisao = input("Digite sua decisão? ")
     if decisao == "1":
-      os.system('clear')
+      os.system('cls')
       break
       
     elif decisao == "2":
-      os.system('clear')
+      os.system('cls')
     else:
       print("digite apenas 1 ou 2!")
       time.sleep(2)
-      os.system('clear')
+      os.system('cls')
 
   else:
     print("Opção inválida. Tente novamente.")
     time.sleep(1)
-    os.system('clear')
+    os.system('cls')
 
 
 
